deeed58cf315abdd3259f1e0e6409d5926f3fe8f
Josh Greenwood <31244208+joshgreenwood2003@users.noreply.github.com>
2022-11-19 13:26:56 +0000

    Add files via upload

---
+++ TestSender/TestSender/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace TestSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("localhost", 6969);
+            Console.ReadLine();
+
+        }
+    }
+}
